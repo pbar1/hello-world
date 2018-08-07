7bd0ecc7db5c4202c527fe4e9b2df979c12d6a46
Pierce Bartine <piercebartine@gmail.com>
2018-08-07 18:54:52 -0500

    feat(aur): added AUR deploy step

---
+++ ci/aur/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+cd "$TRAVIS_BUILD_DIR/ci/aur"
+
+mkdir -p "$HOME/.ssh"
+cat sshconfig >> "$HOME/.ssh/config"
+openssl aes-256-cbc -K $encrypted_0df9e98ee504_key -iv $encrypted_0df9e98ee504_iv -in aur.enc -out "$HOME/.ssh/aur" -d
+ssh-keyscan aur.archlinux.org >> "$HOME/.ssh/known_hosts"
+
+git clone ssh://aur@aur.archlinux.org/pbar-hello-world-bin.git
+
+export VERSION=$(echo "$TRAVIS_TAG" | tr -d v)
+sed -i "s/VERSION/$VERSION/g" PKGBUILD .SRCINFO
+
+curl -s https://api.github.com/repos/pbar1/hello-world/releases/$TRAVIS_TAG \
+| grep "https://github.com/pbar1/hello-world/releases/download" \
+| grep "linux" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - -O hello-world
+
+export CHECKSUM=$(sha256sum hello-world | cut -d ' ' -f 1)
+sed -i "s/CHECKSUM/$CHECKSUM/g" PKGBUILD .SRCINFO
+
+git git add PKGBUILD .SRCINFO
+git commit -m "Deployed $TRAVIS_TAG from Travis CI"
+git push
