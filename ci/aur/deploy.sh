78b2fe635ea3c6c57ae07849e464a67506933203
Pierce Bartine <piercebartine@gmail.com>
2018-08-07 19:59:46 -0500

    fix(aur): comment out all git stuff

--- ci/aur/deploy.sh
+++ ci/aur/deploy.sh
@@ -3,22 +3,22 @@ set -e
 
 cd "$TRAVIS_BUILD_DIR/ci/aur"
 
-# cat "./sshconfig" >> "$HOME/.ssh/config"
+cat "./sshconfig" >> "$HOME/.ssh/config"
 
-git clone ssh://aur@aur.archlinux.org/pbar-hello-world-bin.git
+# git clone ssh://aur@aur.archlinux.org/pbar-hello-world-bin.git
 
-# export VERSION=$(echo "$TRAVIS_TAG" | tr -d v)
-# sed -i "s/VERSION/$VERSION/g" PKGBUILD .SRCINFO
+export VERSION=$(echo "$TRAVIS_TAG" | tr -d v)
+sed -i "s/VERSION/$VERSION/g" PKGBUILD .SRCINFO
 
-# curl -s https://api.github.com/repos/pbar1/hello-world/releases/$TRAVIS_TAG \
-# | grep "https://github.com/pbar1/hello-world/releases/download" \
-# | grep "linux" \
-# | cut -d : -f 2,3 \
-# | tr -d \" \
-# | wget -qi - -O hello-world
+curl -s https://api.github.com/repos/pbar1/hello-world/releases/$TRAVIS_TAG \
+| grep "https://github.com/pbar1/hello-world/releases/download" \
+| grep "linux" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - -O hello-world
 
-# export CHECKSUM=$(sha256sum hello-world | cut -d ' ' -f 1)
-# sed -i "s/CHECKSUM/$CHECKSUM/g" PKGBUILD .SRCINFO
+export CHECKSUM=$(sha256sum hello-world | cut -d ' ' -f 1)
+sed -i "s/CHECKSUM/$CHECKSUM/g" PKGBUILD .SRCINFO
 
 # cp PKGBUILD pbar-hello-world
 # cp .SRCINFO pbar-hello-world
