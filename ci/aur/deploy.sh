c25a3fca5386f52d58c8190d621dbe85082943db
Pierce Bartine <piercebartine@gmail.com>
2018-08-07 20:06:55 -0500

    fix(aur): chmod +x deploy.sh

--- ci/aur/deploy.sh
+++ ci/aur/deploy.sh
@@ -3,12 +3,9 @@ set -e
 
 cd "$TRAVIS_BUILD_DIR/ci/aur"
 
-cat "./sshconfig" >> "$HOME/.ssh/config"
+cat sshconfig >> "$HOME/.ssh/config"
 
-# git clone ssh://aur@aur.archlinux.org/pbar-hello-world-bin.git
-
-export VERSION=$(echo "$TRAVIS_TAG" | tr -d v)
-sed -i "s/VERSION/$VERSION/g" PKGBUILD .SRCINFO
+git clone ssh://aur@aur.archlinux.org/pbar-hello-world-bin.git
 
 curl -s https://api.github.com/repos/pbar1/hello-world/releases/$TRAVIS_TAG \
 | grep "https://github.com/pbar1/hello-world/releases/download" \
@@ -17,12 +14,16 @@ curl -s https://api.github.com/repos/pbar1/hello-world/releases/$TRAVIS_TAG \
 | tr -d \" \
 | wget -qi - -O hello-world
 
+export VERSION=$(echo "$TRAVIS_TAG" | tr -d v)
+sed -i "s/VERSION/$VERSION/g" PKGBUILD .SRCINFO
+
 export CHECKSUM=$(sha256sum hello-world | cut -d ' ' -f 1)
 sed -i "s/CHECKSUM/$CHECKSUM/g" PKGBUILD .SRCINFO
 
-# cp PKGBUILD pbar-hello-world
-# cp .SRCINFO pbar-hello-world
-# cd pbar-hello-world
-# git git add PKGBUILD .SRCINFO
-# git commit -m "Deployed $TRAVIS_TAG from Travis CI"
-# git push
+cp PKGBUILD pbar-hello-world
+cp .SRCINFO pbar-hello-world
+cd pbar-hello-world
+
+git git add PKGBUILD .SRCINFO
+git commit -m "Deployed $TRAVIS_TAG from Travis CI"
+git push
